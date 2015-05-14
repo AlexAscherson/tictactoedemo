ea9bbb8fe2e8d810ea603fc3f75788bfcaa60d34
MacBook <MacBook@MacBooks-MacBook-Pro.local>
2015-05-14 11:30:28 +0100

    cleaned up after merge

--- ticv2.js
+++ ticv2.js
@@ -1,8 +1,11 @@
 var activeplayer = "x";
 var xmoves = [];
 var ymoves = [];
-var turn = 0;
+
 var playerwin = "none yet";
+
+var turn = 0;
+
 var clicked_id = "";
 
 var arraytoCheck = [];
@@ -13,109 +16,107 @@ $( document ).ready(function() {
 
 
 
-function checkforwinner(){
+  function checkforwinner(){
 
-console.log("checkingforwinner");
+    console.log("checkingforwinner");
+    //set array to check
+    if (activeplayer === 'x'){
+      arraytoCheck = xmoves;
+    } else{
+      arraytoCheck = ymoves;
+    }
+    // console.log(arraytoCheck);
+     //debugger;
 
-//set array to check
-
-if (activeplayer === 'x'){
-  arraytoCheck = xmoves;
-
-} else{
-  arraytoCheck = ymoves;
-}
-// console.log(arraytoCheck);
-//debugger;
-
-//begin win checker
-for (var i = 0; i < arrwinPositions.length; i++) 
-//start loop and run the winning values array as many times as there are items in the array 
-  {    
-    // console.log("started first loop" +i);
-   
-    for(k = 0; k < arraytoCheck.length; k++){
-       // console.log("started second loop" +k);
-      //console.log(this.value);
-      
-      //loop check every value in the array of moves
-      // console.log("at first if check" + arraytoCheck);
-            // console.log("against" + arrwinPositions);
-            console.log('am i true? ... ' + typeof arrwinPositions[i][0] + ' ' + typeof arraytoCheck[k]);
+    //begin win checker
+    for (var i = 0; i < arrwinPositions.length; i++) 
+    //start loop and run the winning values array as many times as there are items in the array 
+    {    
+      // console.log("started first loop" +i);
+     
+      for(k = 0; k < arraytoCheck.length; k++){
+         // console.log("started second loop" +k);
+        //console.log(this.value);
+        
+        //loop check every value in the array of moves
+        // console.log("at first if check" + arraytoCheck);
+              // console.log("against" + arrwinPositions);
+              console.log('am i true? ... ' + typeof arrwinPositions[i][0] + ' ' + typeof arraytoCheck[k]);
 
 
-      if (arrwinPositions[i][0] === arraytoCheck[k]){
-         console.log("firstif found" +i +k);
-         // debugger;
-         
-        //check if the first value of the current (i) arrwinPositions(move?) is equal to current value of the WinArray
-        if (arrwinPositions[i][1]=== arraytoCheck[k+1] && arrwinPositions[i][2] === arraytoCheck[k+2]){
-          //console.log("secondif " +i +" "+k);
-          alert( activeplayer + ' is a winner');
-          playerwin = activeplayer;
-          
-          break;
+        if (arrwinPositions[i][0] === arraytoCheck[k]){
+           console.log("firstif found" +i +k);
+           // debugger;
+           
+          //check if the first value of the current (i) arrwinPositions(move?) is equal to current value of the WinArray
+          if (arrwinPositions[i][1]=== arraytoCheck[k+1] && arrwinPositions[i][2] === arraytoCheck[k+2]){
+            //console.log("secondif " +i +" "+k);
+            alert( activeplayer + ' is a winner');
+            playerwin = activeplayer;
+            
+            break;
 
+          }
         }
       }
     }
   }
-}
-//end winchecker
+    //end winchecker function
 
  $( ".gamesquare" ).click(function() {
       //get id of clicked element
 
-      clicked_id = $(this).attr('id');
-      console.log(clicked_id);
-      turn = turn +1;
+    clicked_id = $(this).attr('id');
+    console.log(clicked_id);
+    turn = turn +1;
 
         // check box is empty
-        if ($(this).text() === "") {
+    if ($(this).text() === "") 
+    {
 
-          //check what player it is
+        //check what player it is
 
-          if (activeplayer === "x") {
-              //write x into box
-            $(this).text("x");
+      if (activeplayer === "x") 
+      {
+          //write x into box
+        $(this).text("x");
+        
+        //add played move to moves array
+          xmoves.push(Number(this.id));
+          //run check for winner function
+          checkforwinner();
+          //change active player
+          activeplayer = "y";
 
-             //then change active player
+        //end player1 code
+
+      } else 
+        {
+        
+          $(this).text("y");
+          ymoves.push(Number(this.id));
+          checkforwinner();
+          activeplayer = "x";
+        } 
+              //endplayer2code /else
             
-            //add played move to moves array
-              xmoves.push(Number(this.id));
-              //run check for winner function
-              checkforwinner();
-              //change active player
-              activeplayer = "y";
-
-            //end player1 code
-
-          } else {
-            
-              $(this).text("y");
-              ymoves.push(Number(this.id));
-              checkforwinner();
-              activeplayer = "x";
-              
-                //endplayer2code /else
-           }
 
             //end emptycheck 
-        } else {console.log("thing is full");}
-
-
+    } else {console.log("thing is full");}
           //endelse 
-     }
 
-
-
+    if (turn >= 9 && playerwin ==="none yet"){
+      alert("its a draw");
+      console.log("checking for draw");
+    };
+  }
     //endclickfunction
-    ); 
 
-if (turn >= 9 && playerwin ==="none yet"){
-  alert("its a draw");
-  console.log("checking for draw");
-};
 
-  //end docuready
-   });
+      ); 
+//what is this one doing?
+
+    
+
+    //end docuready
+});
